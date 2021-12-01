8d2a6a917f465994a7c744d8f0dc47da8ca22c05
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-01 16:24:46 +0100

    Add Day01
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/01/index.test.ts
+++ 2021/01/index.test.ts
@@ -3,8 +3,19 @@ import { expect } from "chai";
 import { main } from "./index";
 
 describe("day01", () => {
-    it("should return ???", () => {
-        const result = main();
-        expect(result).to.equal(undefined);
+    it("should return 7 and 5", () => {
+        const result = main(`199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+        `);
+        expect(result[0]).to.equal(7);
+        expect(result[1]).to.equal(5);
     });
 });
--- 2021/01/index.ts
+++ 2021/01/index.ts
@@ -1,5 +1,26 @@
 // day01:
+import { readFileSync, writeFileSync } from "fs";
+import { resolve } from "path";
 
-export function main() {
-    return undefined;
+const inputPath = resolve(__dirname, "./input.txt");
+const outputPath = resolve(__dirname, "./output.txt");
+
+const input = readFileSync(inputPath, "utf8");
+
+writeFileSync(outputPath, JSON.stringify(main(input)), "utf8");
+
+export function main(input: string) {
+    const nums = input.split("\n").map((i) => parseInt(i, 10));
+    const res1 = nums.filter((v, i, arr) => (arr[i - 1] ?? 999999999) < v);
+
+    const res2 = nums
+        .map(
+            (v, i, arr) =>
+                (arr[i - 1] ?? -999999999) + v + (arr[i + 1] ?? -999999999),
+        )
+        .map((v) => (isNaN(v) ? 0 : v === Math.abs(v) ? v : 0))
+        .splice(1)
+        .filter((v, i, arr) => (arr[i - 1] ?? 999999999) < v);
+
+    return [res1.length, res2.length];
 }
