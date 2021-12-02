c25aad9bf55ce55217813d43262a5ca893674652
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-02 06:30:41 +0100

    Add Day02
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/02/index.test.ts
+++ 2021/02/index.test.ts
@@ -1,17 +1,31 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day02", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { stage0, stage1 } from "./index";
+
+describe("day02", () => {
+    it("stage 0: This sequence of commands should dive the submarine to 150m", () => {
+        const input = [
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2",
+        ];
+        const res = stage0(input);
+        expect(res).to.equal(150);
+    });
+
+    it("stage 1: If the sub uses aim, this sequence of should dive the submarine to 900m", () => {
+        const input = [
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2",
+        ];
+        const res = stage1(input);
+        expect(res).to.equal(900);
+    });
+});
--- 2021/02/index.ts
+++ 2021/02/index.ts
@@ -1,19 +1,63 @@
 // day02:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day02: Answer.`)
+    const input = readFileSync(inputPath, "utf8").split("\n");
+    console.log(
+        `day02: The multiple of the final horizontal position by the final depth is ${stage0(
+            input,
+        )}m and ${stage1(input)}m if the submarine uses aim.`,
+    );
 }
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+export function stage0(arg: string[]) {
+    let horizontal = 0;
+    let depth = 0;
+    for (const cmd of arg.map((v) => v.split(" "))) {
+        switch (cmd[0]) {
+            case "forward":
+                horizontal += parseInt(cmd[1] ?? "");
+                break;
+            case "up":
+                depth -= parseInt(cmd[1] ?? "");
+                break;
+            case "down":
+                depth += parseInt(cmd[1] ?? "");
+                break;
+            case "":
+                break;
+            default:
+                console.error("Unknown command");
+                break;
+        }
+    }
+    return horizontal * depth;
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+export function stage1(arg: string[]) {
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
+    for (const cmd of arg.map((v) => v.split(" "))) {
+        switch (cmd[0]) {
+            case "forward":
+                horizontal += parseInt(cmd[1] ?? "");
+                depth += aim * parseInt(cmd[1] ?? "");
+                break;
+            case "up":
+                aim -= parseInt(cmd[1] ?? "");
+                break;
+            case "down":
+                aim += parseInt(cmd[1] ?? "");
+                break;
+            case "":
+                break;
+            default:
+                console.error("Unknown command");
+                break;
+        }
+    }
+    return horizontal * depth;
 }
