31c073bd63e6fe194f6b36cdff4c7120a9dd1538
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-03 23:27:30 +0100

    Add Day03
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/03/index.test.ts
+++ 2021/03/index.test.ts
@@ -1,17 +1,30 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day03", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { stage0, stage1 } from "./index";
+
+describe("day03", () => {
+    const input = [
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010",
+    ].map((v) => v.split(""));
+    console.log(input);
+    it("Not defiend yet", () => {
+        const res = stage0(input);
+        expect(res).to.equal(198);
+    });
+
+    it("Not defiend yet", () => {
+        const res = stage1(input);
+        expect(res).to.equal(230);
+    });
+});
--- 2021/03/index.ts
+++ 2021/03/index.ts
@@ -1,19 +1,77 @@
 // day03:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day03: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = readFileSync(inputPath, "utf8")
+        .split("\n")
+        .map((v) => v.split(""));
+    console.log(
+        `day03: ${stage0(input)} is the power consumption and ${stage1(
+            input,
+        )} the life support rating of the submarine `,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+export function stage0(arg: string[][]) {
+    arg.map((v) => v.reverse());
+    const len = arg
+        .map((v) => v.length)
+        .reduce((prev, cur) => (prev < cur ? cur : prev));
+
+    const gammaArr: string[] = [];
+    const epsilonArr: string[] = [];
+
+    for (let i = 0; i < len; i++) {
+        gammaArr.push(count(arg, i) < 0 ? "0" : "1");
+        epsilonArr.push(count(arg, i) > 0 ? "0" : "1");
+    }
+
+    const gamma = gammaArr.reverse().join("");
+    const epsilon = epsilonArr.reverse().join("");
+
+    return parseInt(gamma, 2) * parseInt(epsilon, 2);
+}
+
+export function stage1(test: string[][]) {
+    console.log(test.map((v) => v.reverse()));
+    let oxygenArr = test;
+    let co2Arr = test;
+    let i = 0;
+
+    while (oxygenArr.length > 1) {
+        const char = count(oxygenArr, i) >= 0 ? "1" : "0";
+        oxygenArr = oxygenArr.filter((v) => v[i] === char);
+        i++;
+    }
+
+    i = 0;
+
+    while (co2Arr.length > 1) {
+        const char = count(co2Arr, i) < 0 ? "1" : "0";
+        co2Arr = co2Arr.filter((v) => v[i] === char);
+        i++;
+    }
+
+    console.log(`${oxygenArr[0]?.join("")}+${co2Arr[0]?.join("")}`);
+
+    return (
+        parseInt(oxygenArr[0]?.join("") ?? "", 2) *
+        parseInt(co2Arr[0]?.join("") ?? "", 2)
+    );
+}
+
+function count(arr: string[][], pos: number) {
+    let res = 0;
+    for (const str of arr) {
+        if (str[pos] === "0") {
+            res--;
+        }
+        if (str[pos] === "1") {
+            res++;
+        }
+    }
+    return res;
 }
