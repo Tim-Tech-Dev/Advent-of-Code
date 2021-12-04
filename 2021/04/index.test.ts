ce9e4985f2ebd9d9bb36fa02364e2281baf4c417
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-04 17:39:57 +0100

    Add Day04
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/04/index.test.ts
+++ 2021/04/index.test.ts
@@ -1,17 +1,35 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day04", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { stage0, stage1, parse } from "./index";
+
+describe("day04", () => {
+    const input =
+        parse(`7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7`);
+    it("Bingo! Our final score should be 4512", () => {
+        const res = stage0(input.nums, input.boards);
+        expect(res).to.equal(4512);
+    });
+
+    it("If the squid must win, we should get a score of 1924", () => {
+        const res = stage1(input.nums, input.boards);
+        expect(res).to.equal(1924);
+    });
+});
--- 2021/04/index.ts
+++ 2021/04/index.ts
@@ -1,19 +1,120 @@
 // day04:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day04: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = parse(readFileSync(inputPath, "utf8"));
+    console.log(
+        `Bingo! Our final score is ${stage0(
+            input.nums,
+            input.boards,
+        )} and if the squid should win we get a score of ${stage1(
+            input.nums,
+            input.boards,
+        )}.`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+export function stage0(nums: number[], boards: number[][][]) {
+    let lastNum = -1;
+    let winningBoard = -1;
+
+    for (const num of nums) {
+        boards = boards.map((board) =>
+            board.map((line) => line.filter((v) => v !== num)),
+        );
+
+        const winnig = boards.findIndex((board) =>
+            board.some((line) => line.length === 0),
+        );
+
+        if (winnig !== -1) {
+            lastNum = num;
+            winningBoard = winnig;
+            break;
+        }
+    }
+
+    const board = boards[winningBoard]?.slice(0, 5);
+    const score = lastNum * sum(board);
+
+    return score;
+}
+
+export function stage1(nums: number[], boards: number[][][]) {
+    let lastNum = -1;
+
+    for (const num of nums) {
+        // remove numbers drawn
+        boards = boards.map((board) =>
+            board.map((line) => line.filter((v) => v !== num)),
+        );
+
+        // filter out winning boards
+        if (boards.length > 1) {
+            boards = boards.filter(
+                (board) => !board.some((line) => line.length === 0),
+            );
+        }
+
+        // check if last board won
+        if (boards[0] && boards[0].some((line) => line.length === 0)) {
+            lastNum = num;
+            break;
+        }
+    }
+
+    const board = boards[0]?.slice(0, 5);
+    const score = lastNum * sum(board);
+
+    return score;
+}
+
+export function parse(str: string) {
+    const temp = str.split("\n\n");
+    const nums =
+        temp
+            .shift()
+            ?.split(",")
+            .map((v) => parseInt(v)) ?? [];
+    let boards = temp.map((i) =>
+        i.split("\n").map((j) =>
+            j
+                .split(" ")
+                .filter((k) => k !== "")
+                .map((l) => parseInt(l)),
+        ),
+    );
+
+    // generate all possible columns and rows
+    boards = boards.map((board) =>
+        board.concat(
+            [0, 1, 2, 3, 4].map((i) => [
+                (board[0] ?? [])[i] ?? -1,
+                (board[1] ?? [])[i] ?? -1,
+                (board[2] ?? [])[i] ?? -1,
+                (board[3] ?? [])[i] ?? -1,
+                (board[4] ?? [])[i] ?? -1,
+            ]),
+        ),
+    );
+    return {
+        nums: nums,
+        boards: boards,
+    };
+}
+
+function sum(board?: number[][]) {
+    const res = board
+        ?.map((v) => {
+            if (v.length) {
+                return v.reduce((a, b) => a + b);
+            } else {
+                return 0;
+            }
+        })
+        .reduce((a, b) => a + b);
+    return res ? res : 0;
 }
