01fb33423d86df1da5ecab39282dfd4ffbfd8a83
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-05 10:08:07 +0100

    Add Day05
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/05/index.test.ts
+++ 2021/05/index.test.ts
@@ -1,17 +1,27 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day05", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { parse, stage0, stage1 } from "./index";
+
+describe("day05", () => {
+    const input = parse(`0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`);
+    it("Not defiend yet", () => {
+        const res = stage0(input);
+        expect(res).to.equal(5);
+    });
+
+    it("Not defiend yet", () => {
+        const res = stage1(input);
+        expect(res).to.equal(12);
+    });
+});
+
+
--- 2021/05/index.ts
+++ 2021/05/index.ts
@@ -1,19 +1,113 @@
 // day05:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day05: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = parse(readFileSync(inputPath, "utf8"));
+    console.log(
+        `There are '${stage0(
+            input,
+        )}' points at witch at least two lines overlap and '${stage1(
+            input,
+        )}' with diagonals.`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+type LineDefinition = {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+};
+
+export function stage0(lines: LineDefinition[]) {
+    const points: [number, number][] = [];
+    const counts: { [index: string]: number } = {};
+
+    for (const c of lines) {
+        const x = Math.min(c.x1, c.x2);
+        const xm = Math.max(c.x1, c.x2);
+        const y = Math.min(c.y1, c.y2);
+        const ym = Math.max(c.y1, c.y2);
+
+        // horizontal and vertical lines
+        if (xm - x === 0 || ym - y === 0) {
+            for (let i = x; i <= xm; i++) {
+                for (let j = y; j <= ym; j++) {
+                    points.push([i, j]);
+                }
+            }
+        }
+    }
+
+    points
+        .map((v) => v.toString())
+        .forEach(function (x) {
+            counts[x] = (counts[x] || 0) + 1;
+        });
+
+    return Object.values(counts).filter((v) => v > 1).length;
+}
+
+export function stage1(lines: LineDefinition[]) {
+    const points: [number, number][] = [];
+    const counts: { [index: string]: number } = {};
+
+    for (const c of lines) {
+        const x = Math.min(c.x1, c.x2);
+        const xm = Math.max(c.x1, c.x2);
+        const y = Math.min(c.y1, c.y2);
+        const ym = Math.max(c.y1, c.y2);
+
+        // horizontal and vertical lines
+        if (xm - x === 0 || ym - y === 0) {
+            for (let i = x; i <= xm; i++) {
+                for (let j = y; j <= ym; j++) {
+                    points.push([i, j]);
+                }
+            }
+        }
+
+        // diagonal lines
+        if (xm - x === ym - y) {
+            if ((c.x1 < c.x2 && c.y1 < c.y2) || (c.x1 > c.x2 && c.y1 > c.y2)) {
+                for (let i = 0; i <= xm - x; i++) {
+                    points.push([x + i, y + i]);
+                }
+            } else {
+                for (let i = 0; i <= xm - x; i++) {
+                    points.push([xm - i, y + i]);
+                }
+            }
+        }
+    }
+
+    points
+        .map((v) => v.toString())
+        .forEach(function (x) {
+            counts[x] = (counts[x] || 0) + 1;
+        });
+
+    return Object.values(counts).filter((v) => v > 1).length;
+}
+
+export function parse(str: string): LineDefinition[] {
+    return str
+        .split("\n")
+        .map((v) =>
+            v
+                .replace(/ -> /gm, ",")
+                .split(",")
+                .map((v) => parseInt(v)),
+        )
+        .map((v) => {
+            return {
+                x1: v[0] ?? -1,
+                y1: v[1] ?? -1,
+                x2: v[2] ?? -1,
+                y2: v[3] ?? -1,
+            };
+        });
 }
