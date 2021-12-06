28c9d55e60b824c028d7b65d3852fa12de4159e7
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-06 07:42:17 +0100

    Add Day06
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/06/index.test.ts
+++ 2021/06/index.test.ts
@@ -1,17 +1,17 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day06", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { stage } from "./index";
+
+describe("day06", () => {
+    it("Not defiend yet", () => {
+        const input = [3, 4, 3, 1, 2];
+        expect(stage(input, 18)).to.equal(26);
+        expect(stage(input, 80)).to.equal(5934);
+    });
+
+    it("Not defiend yet", () => {
+        const input = [3, 4, 3, 1, 2];
+        const res = stage(input, 256);
+        expect(res).to.equal(26984457539);
+    });
+});
--- 2021/06/index.ts
+++ 2021/06/index.ts
@@ -1,19 +1,44 @@
-// day06:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+// --- Day 6: Lanternfish ---
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day06: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = readFileSync(inputPath, "utf8")
+        .split(",")
+        .map((v) => parseInt(v));
+    console.log(
+        `There will be '${stage(
+            input,
+            80,
+        )}' glowing lanternfish after 80 days and '${stage(
+            input,
+            256,
+        )}' after 256 days, if our models are correct.`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+export function stage(arg: number[], days: number) {
+    let fish: number[] = Array<number>(8).fill(0);
+    arg.map((v) => fish[v]++);
+
+    for (let i = 0; i < days; i++) {
+        fish = day(fish);
+    }
+    return fish.reduce((a, b) => a + b);
+}
+
+function day(fish: number[]) {
+    const newFish: number[] = Array<number>(9).fill(0);
+    for (let i = 0; i < fish.length; i++) {
+        const val = fish[i] ?? 0;
+        if (i === 0) {
+            newFish[6] += val;
+            newFish[8] += val;
+        } else {
+            newFish[i - 1] += val;
+        }
+    }
+    return newFish;
 }
