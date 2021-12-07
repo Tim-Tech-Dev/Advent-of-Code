c33c68da3736d0b62be7a3eb00c32cadd7f83e1e
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-07 10:31:20 +0100

    Add Day07
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/07/index.test.ts
+++ 2021/07/index.test.ts
@@ -1,17 +1,18 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day07", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { stage0, stage1 } from "./index";
+
+describe("[2021-07]", () => {
+    const input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+    it("stage 0: Horizontly align those crab subs should take them 37 fuel", () => {
+        const res = stage0(input);
+        expect(res?.pos).to.equal(2);
+        expect(res?.fuelcost).to.equal(37);
+    });
+
+    it("stage 1: If the crabs do not burn fuel at a constant rate they alig with 168", () => {
+        const res = stage1(input);
+        expect(res?.pos).to.equal(5);
+        expect(res?.fuelcost).to.equal(168);
+    });
+});
--- 2021/07/index.ts
+++ 2021/07/index.ts
@@ -1,19 +1,58 @@
-// day07:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+// --- Day 7: The Treachery of Whales ---
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day07: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = readFileSync(inputPath, "utf8")
+        .split(",")
+        .map((v) => parseInt(v));
+    const res0 = stage0(input);
+    const res1 = stage1(input);
+    console.log(
+        `We have to move the crab submarines to ${res0?.pos} to accive the lowest fuel cost of '${res0?.fuelcost}' and '${res1?.fuelcost}' if the crabs do not burn fuel at a constant rate.`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+export function stage0(arg: number[]) {
+    const arrays: number[][] = Array<number[]>(Math.max(...arg)).fill(arg);
+
+    const costs = arrays
+        .map((v, i) => ({
+            pos: i,
+            fuelcost: v
+                .map((v) => Math.abs(v - i))
+                .reduce((prev, curr) => prev + curr),
+        }))
+        .sort((a, b) => a.fuelcost - b.fuelcost);
+    return costs[0];
+}
+
+export function stage1(arg: number[]) {
+    const arrays: number[][] = Array<number[]>(Math.max(...arg)).fill(arg);
+
+    const costs = arrays
+        .map((v, i) => ({
+            pos: i,
+            fuelcost: v
+                .map((v) => triangular(Math.abs(v - i)))
+                .reduce((prev, curr) => prev + curr),
+        }))
+        .sort((a, b) => a.fuelcost - b.fuelcost);
+    return costs[0];
+}
+
+/**
+ * calculate triangular numbers
+ * (https://en.wikipedia.org/wiki/Triangular_number)
+ *
+ *
+ * Source: https://gist.github.com/ryansmith94/4953830
+ * @param value
+ * @returns
+ */
+function triangular(value: number) {
+    const abs = Math.abs(value);
+    return (abs / 2) * (abs + 1) * (abs / value) || 0;
 }
