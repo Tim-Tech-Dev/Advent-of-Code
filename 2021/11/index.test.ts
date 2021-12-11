7fcb3ce2f8582d6f207bf09f9fe63d378904da98
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-11 12:46:11 +0100

    Add Day11
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/11/index.test.ts
+++ 2021/11/index.test.ts
@@ -1,17 +1,51 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day11", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { stage0, stage1, step } from "./index";
+
+const input = [
+    5, 4, 8, 3, 1, 4, 3, 2, 2, 3, 2, 7, 4, 5, 8, 5, 4, 7, 1, 1, 5, 2, 6, 4, 5,
+    5, 6, 1, 7, 3, 6, 1, 4, 1, 3, 3, 6, 1, 4, 6, 6, 3, 5, 7, 3, 8, 5, 4, 7, 8,
+    4, 1, 6, 7, 5, 2, 4, 6, 4, 5, 2, 1, 7, 6, 8, 4, 1, 7, 2, 1, 6, 8, 8, 2, 8,
+    8, 1, 1, 3, 4, 4, 8, 4, 6, 8, 4, 8, 5, 5, 4, 5, 2, 8, 3, 7, 5, 1, 5, 2, 6,
+];
+
+describe("[2021-11]", () => {
+    it("Testing the stepping function", () => {
+        // one flash
+        const res1 = step([0, 0, 0, 0, 9, 0, 0, 0, 0], 3);
+        expect(res1.octopuses).to.eql([2, 2, 2, 2, 0, 2, 2, 2, 2]);
+        expect(res1.flashes).to.equal(1);
+
+        // multiple flashes
+        const input = [
+            8, 8, 0, 7, 4, 7, 6, 5, 5, 5, 5, 0, 8, 9, 0, 8, 7, 0, 5, 4, 8, 5, 9,
+            7, 8, 8, 9, 6, 0, 8, 8, 4, 8, 5, 7, 6, 9, 6, 0, 0, 8, 7, 0, 0, 9, 0,
+            8, 8, 0, 0, 6, 6, 0, 0, 0, 8, 8, 9, 8, 9, 6, 8, 0, 0, 0, 0, 5, 9, 4,
+            3, 0, 0, 0, 0, 0, 0, 7, 4, 5, 6, 9, 0, 0, 0, 0, 0, 0, 8, 7, 6, 8, 7,
+            0, 0, 0, 0, 6, 8, 4, 8,
+        ];
+        const output = [
+            0, 0, 5, 0, 9, 0, 0, 8, 6, 6, 8, 5, 0, 0, 8, 0, 0, 5, 7, 5, 9, 9, 0,
+            0, 0, 0, 0, 0, 3, 9, 9, 7, 0, 0, 0, 0, 0, 0, 4, 1, 9, 9, 3, 5, 0, 8,
+            0, 0, 6, 3, 7, 7, 1, 2, 3, 0, 0, 0, 0, 0, 7, 9, 1, 1, 2, 5, 0, 0, 0,
+            9, 2, 2, 1, 1, 1, 3, 0, 0, 0, 0, 0, 4, 2, 1, 1, 2, 5, 0, 0, 0, 0, 0,
+            2, 1, 1, 1, 9, 0, 0, 0,
+        ];
+        const res2 = step(input, 10);
+        expect(res2.octopuses).to.eql(output);
+        expect(res2.flashes).to.equal(45);
+    });
+
+    it("stage 0: The number of flashes should be 204 after 10 and 1656 after 100 iterations", () => {
+        const res1 = stage0(input, 10, 10);
+        expect(res1).to.equal(204);
+
+        const res2 = stage0(input, 10, 100);
+        expect(res2).to.equal(1656);
+    });
+
+    it("stage 1: The synchronizing should happen at step 195", () => {
+        const res = stage1(input, 10);
+        expect(res).to.equal(195);
+    });
+});
--- 2021/11/index.ts
+++ 2021/11/index.ts
@@ -1,19 +1,117 @@
-// day11:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+// --- Day 11: Dumbo Octopus ---
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day11: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = readFileSync(inputPath, "utf8")
+        .split("")
+        .filter((v) => v !== "\n")
+        .map((v) => parseInt(v));
+    console.log(
+        `The octopuses flash '${stage0(
+            input,
+            10,
+            100,
+        )}' times in total after 100 steps. They will synchronize in step '${stage1(
+            input,
+            10,
+        )}'.`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+function adjacent<T, R>(
+    array: T[],
+    width: number,
+    pos: number,
+    callbackfn: (value: T, index: number, array: T[]) => R,
+): R[] {
+    const res: R[] = [];
+
+    const locations = [
+        pos - width,
+        pos - width + 1,
+        pos + 1,
+        pos + width + 1,
+        pos + width,
+        pos + width - 1,
+        pos - 1,
+        pos - width - 1,
+    ];
+
+    locations.forEach((v) => {
+        const item = array[v];
+        if (item && v >= 0 && v < array.length) {
+            if (v - pos > 1) {
+                if (Math.floor(pos / width) + 1 === Math.floor(v / width)) {
+                    res.push(callbackfn(item, v, array));
+                }
+            } else if (v - pos < -1) {
+                if (Math.floor(pos / width) - 1 === Math.floor(v / width)) {
+                    res.push(callbackfn(item, v, array));
+                }
+            } else {
+                if (Math.floor(pos / width) === Math.floor(v / width)) {
+                    res.push(callbackfn(item, v, array));
+                }
+            }
+        }
+    });
+
+    return res;
+}
+
+export function step(
+    octopuses: number[],
+    width: number,
+): { octopuses: number[]; flashes: number } {
+    // step A
+    octopuses = octopuses.map((octopus) => octopus + 1);
+
+    // step B
+    let flashes = -1;
+    let newflashes = 0;
+    while (flashes !== newflashes) {
+        flashes = newflashes;
+        octopuses.forEach((octopus, i) => {
+            if (octopus > 9) {
+                newflashes++;
+                adjacent(octopuses, width, i, (_v, i, arr) => arr[i]++);
+                octopuses[i] = Number.NEGATIVE_INFINITY;
+            }
+        });
+    }
+
+    // step C
+    octopuses = octopuses.map((octopus) => (octopus < 0 ? 0 : octopus));
+
+    return { octopuses, flashes };
+}
+
+export function stage0(octopuses: number[], width: number, iterations: number) {
+    let flashes = 0;
+
+    for (let i = 0; i < iterations; i++) {
+        const res = step(octopuses, width);
+        octopuses = res.octopuses;
+        flashes += res.flashes;
+    }
+
+    return flashes;
+}
+
+export function stage1(octopuses: number[], width: number) {
+    let steps = 0;
+
+    let flashes = 0;
+
+    while (flashes !== 100) {
+        const res = step(octopuses, width);
+        octopuses = res.octopuses;
+        flashes = res.flashes;
+        steps++;
+    }
+
+    return steps;
 }
