255753f7f7662cc44e168ad16bb307b9af0679ee
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-12 11:58:12 +0100

    Add Day12
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/12/index.test.ts
+++ 2021/12/index.test.ts
@@ -1,17 +1,34 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day12", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { stage0, stage1 } from "./index";
+
+const input = `fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`.split("\n");
+
+describe("[2021-12]", () => {
+    it("stage 0: There should be 226 possible paths", () => {
+        const res = stage0(input);
+        expect(res).to.equal(226);
+    });
+
+    it("stage 1: If we travel to one small cave twice, there should be 3509 possible paths", () => {
+        const res = stage1(input);
+        expect(res).to.equal(3509);
+    });
+});
--- 2021/12/index.ts
+++ 2021/12/index.ts
@@ -1,19 +1,94 @@
-// day12:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+// --- Day 12: Passage Pathing ---
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day12: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = readFileSync(inputPath, "utf8").split("\n");
+    console.log(
+        ` There are '${stage0(
+            input,
+        )}' possible paths. If we travel to one small cave twice, there are '${stage1(
+            input,
+        )}' possible paths.`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+function recursion(
+    nodes: {
+        [index: string]: string[];
+    },
+    trace: string[],
+    curr: string,
+    ignoreOnce?: string,
+): string[][] | undefined {
+    if (curr === "end") {
+        return [trace.concat(curr)];
+    }
+
+    if (
+        curr === ignoreOnce &&
+        trace.indexOf(curr) === trace.lastIndexOf(curr)
+    ) {
+        return nodes[curr]
+            ?.map((str) => recursion(nodes, [...trace, curr], str, ignoreOnce))
+            .reduce((prev, next) => (prev ?? []).concat(next ?? []));
+    }
+
+    if (curr.toLowerCase() === curr && trace.includes(curr)) {
+        return undefined;
+    }
+
+    return nodes[curr]
+        ?.map((str) => recursion(nodes, [...trace, curr], str, ignoreOnce))
+        .reduce((prev, next) => (prev ?? []).concat(next ?? []));
+}
+
+export function stage0(arg: string[]) {
+    const nodes: { [index: string]: string[] } = {};
+    const edges = arg.map((v) => v.split("-"));
+
+    for (const edge of edges) {
+        const a = edge[0];
+        const b = edge[1];
+        if (a && b) {
+            nodes[a] = (nodes[a] || []).concat(b);
+            nodes[b] = (nodes[b] || []).concat(a);
+        }
+    }
+
+    const res = recursion(nodes, [], "start");
+
+    return res?.length;
+}
+
+export function stage1(arg: string[]) {
+    const nodes: { [index: string]: string[] } = {};
+    const counts: { [index: string]: number } = {};
+    const edges = arg.map((v) => v.split("-"));
+    let paths: string[][] = [];
+
+    for (const edge of edges) {
+        const a = edge[0];
+        const b = edge[1];
+        if (a && b) {
+            nodes[a] = (nodes[a] || []).concat(b);
+            nodes[b] = (nodes[b] || []).concat(a);
+        }
+    }
+
+    const smallCaves = Object.keys(nodes).filter(
+        (v) => v.toLowerCase() === v && v !== "start" && v !== "end",
+    );
+
+    for (const smallCave of smallCaves) {
+        paths = paths.concat(recursion(nodes, [], "start", smallCave) ?? []);
+    }
+
+    const strings = paths.map((v) => v.join("-"));
+
+    strings.forEach((v) => (counts[v] = (counts[v] || 0) + 1));
+
+    return Object.keys(counts).length;
 }
