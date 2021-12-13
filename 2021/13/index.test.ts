168981b8ba02b41e0f5c25bd26584de4075e21a4
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-13 20:26:42 +0100

    Add Day13
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/13/index.test.ts
+++ 2021/13/index.test.ts
@@ -1,17 +1,115 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day13", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { fold, parse, stage0, stage1 } from "./index";
+
+const points: [number, number][] = [
+    [6, 10],
+    [0, 14],
+    [9, 10],
+    [0, 3],
+    [10, 4],
+    [4, 11],
+    [6, 0],
+    [6, 12],
+    [4, 1],
+    [0, 13],
+    [10, 12],
+    [3, 4],
+    [3, 0],
+    [8, 4],
+    [1, 10],
+    [2, 14],
+    [8, 10],
+    [9, 0],
+];
+
+const input = parse(`6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`);
+
+describe("[2021-13]", () => {
+    it("test fold function x", () => {
+        const res = fold(
+            [
+                [9, 0],
+                [6, 6],
+            ],
+            ["x", 5],
+        );
+        expect(res).to.deep.equal([
+            [1, 0],
+            [4, 6],
+        ]);
+    });
+
+    it("test fold function y", () => {
+        const res = fold(
+            [
+                [0, 9],
+                [6, 6],
+            ],
+            ["y", 5],
+        );
+        expect(res).to.deep.equal([
+            [0, 1],
+            [6, 4],
+        ]);
+    });
+
+    it("test first fold of stage 1 test", () => {
+        const res = fold(points, ["y", 7]);
+        expect(res).to.deep.equal([
+            [6, 4],
+            [0, 0],
+            [9, 4],
+            [0, 3],
+            [10, 4],
+            [4, 3],
+            [6, 0],
+            [6, 2],
+            [4, 1],
+            [0, 1],
+            [10, 2],
+            [3, 4],
+            [3, 0],
+            [8, 4],
+            [1, 4],
+            [2, 0],
+            [8, 4],
+            [9, 0],
+        ]);
+    });
+
+    it("stage 0: The resulting paper should have 16 points", () => {
+        const res = stage0(input);
+        expect(res).to.equal(16);
+    });
+
+    it("stage 1: The instructions should make a square", () => {
+        const res = stage1(input);
+        expect(res).to.equal(`#####
+#...#
+#...#
+#...#
+#####
+`);
+    });
+});
--- 2021/13/index.ts
+++ 2021/13/index.ts
@@ -1,19 +1,104 @@
-// day13:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+// --- Day 13: Transparent Origami ---
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day13: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = parse(readFileSync(inputPath, "utf8"));
+    console.log(
+        `After the first fold ther are '${stage0({
+            points: input.points,
+            foldingInstructions: [input.foldingInstructions[0] ?? ["", 0]],
+        })}' points visible. The activation code for our infrared thermal imaging camera is:\n'${stage1(
+            {
+                points: input.points,
+                foldingInstructions: input.foldingInstructions,
+            },
+        )}'`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+export function parse(str: string) {
+    const parts = str.split("\n\n");
+    const points = parts[0]
+        ?.split("\n")
+        .map((v) => v.split(",").map((v) => parseInt(v))) as [number, number][];
+    const foldingInstructions =
+        parts[1]
+            ?.split("\n")
+            .map(
+                (v) =>
+                    [v.slice(11, 12), parseInt(v.slice(13))] as [
+                        string,
+                        number,
+                    ],
+            ) ?? [];
+    return {
+        points,
+        foldingInstructions,
+    };
+}
+
+export function fold(
+    points: [number, number][],
+    instruction: [string, number],
+) {
+    const array: [number, number][] = [];
+    points.forEach((point) => {
+        if (point[0] > instruction[1] && instruction[0] === "x") {
+            array.push([2 * instruction[1] - point[0], point[1]]);
+        } else if (point[1] > instruction[1] && instruction[0] === "y") {
+            array.push([point[0], 2 * instruction[1] - point[1]]);
+        } else {
+            array.push(point);
+        }
+    });
+    return array;
+}
+
+export function stage0(arg: {
+    points: [number, number][];
+    foldingInstructions: [string, number][];
+}) {
+    let points = arg.points;
+    const counts: { [index: string]: number } = {};
+
+    for (const instruction of arg.foldingInstructions) {
+        points = fold(points, instruction);
+    }
+
+    points.forEach(
+        (v) => (counts[v.join(",")] = (counts[v.join(",")] || 0) + 1),
+    );
+
+    return Object.keys(counts).length;
+}
+
+export function stage1(arg: {
+    points: [number, number][];
+    foldingInstructions: [string, number][];
+}) {
+    let points = arg.points;
+
+    for (const instruction of arg.foldingInstructions) {
+        points = fold(points, instruction);
+    }
+
+    const max = points.reduce((a, b) => [
+        Math.max(a[0], b[0] + 1),
+        Math.max(a[1], b[1] + 1),
+    ]);
+    let str = "";
+    for (let y = 0; y < max[1]; y++) {
+        for (let x = 0; x < max[0]; x++) {
+            if (points.map((v) => v.join()).includes(`${x},${y}`)) {
+                str += "#";
+            } else {
+                str += ".";
+            }
+        }
+        str += "\n";
+    }
+    return str;
 }
