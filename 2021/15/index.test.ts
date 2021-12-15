bfd021fe71487accf72fc3565ae0ba4b3b4b8510
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-15 23:55:25 +0100

    Add Day15
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- 2021/15/index.test.ts
+++ 2021/15/index.test.ts
@@ -1,17 +1,41 @@
-// import "mocha";
-// import { expect } from "chai";
-// import { stage0, stage1 } from "./index";
-//
-// describe("day15", () => {
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage0(input);
-//         expect(res).to.equal(undefined);
-//     });
-//
-//     it("Not defiend yet", () => {
-//         const input = "";
-//         const res = stage1(input);
-//         expect(res).to.equal(undefined);
-//     });
-// });
+import "mocha";
+import { expect } from "chai";
+import { parse, stage0, stage1 } from "./index";
+
+const string = `1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581`;
+
+const values = [
+    1, 1, 6, 3, 7, 5, 1, 7, 4, 2, 1, 3, 8, 1, 3, 7, 3, 6, 7, 2, 2, 1, 3, 6, 5,
+    1, 1, 3, 2, 8, 3, 6, 9, 4, 9, 3, 1, 5, 6, 9, 7, 4, 6, 3, 4, 1, 7, 1, 1, 1,
+    1, 3, 1, 9, 1, 2, 8, 1, 3, 7, 1, 3, 5, 9, 9, 1, 2, 4, 2, 1, 3, 1, 2, 5, 4,
+    2, 1, 6, 3, 9, 1, 2, 9, 3, 1, 3, 8, 5, 2, 1, 2, 3, 1, 1, 9, 4, 4, 5, 8, 1,
+];
+
+const input = parse(string);
+
+describe("[2021-15]", () => {
+    it("Test parsing function", () => {
+        const res = parse(string);
+        expect(res.width).to.equal(10);
+        expect(res.values).to.deep.equal(values);
+    });
+
+    it("stage 0: The path with the lowest total risk should be 40 units long", () => {
+        const res = stage0(input.values, input.width);
+        expect(res).to.equal(40);
+    });
+
+    it("stage 1: In the expanded map, the path with the lowest total risk should be 315 units long", () => {
+        const res = stage1(input.values, input.width);
+        expect(res).to.equal(315);
+    });
+});
--- 2021/15/index.ts
+++ 2021/15/index.ts
@@ -1,19 +1,171 @@
-// day15:
-// import { readFileSync } from "fs";
-// import { resolve } from "path";
+// --- Day 15: Chiton ---
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// const inputPath = resolve(__dirname, "./input.txt");
+const inputPath = resolve(__dirname, "./input.txt");
 
 export function main() {
-    // const input = readFileSync(inputPath, "utf8");
-    // console.log(`day15: Answer.`)
-}
-export function stage0(arg: any) {
-    arg;
-    return undefined;
+    const input = parse(readFileSync(inputPath, "utf8"));
+    console.log(
+        `The total risk level caused by the chitons is '${stage0(
+            input.values,
+            input.width,
+        )}'. If the map is expanded rhe value increses to '${stage1(
+            input.values,
+            input.width,
+        )}'.`,
+    );
 }
 
-export function stage1(arg: any) {
-    arg;
-    return undefined;
+export function parse(str: string) {
+    const width = Math.max(...str.split("\n").map((v) => v.length));
+    const values = str
+        .replace(/\n/gm, "")
+        .split("")
+        .map((v) => parseInt(v));
+    return { values, width };
+}
+
+function connected<T, R>(
+    array: T[],
+    width: number,
+    pos: number,
+    callbackfn: (value: T, index: number, array: T[]) => R,
+): R[] {
+    const res: R[] = [];
+
+    const locations = [pos - width, pos + 1, pos + width, pos - 1];
+
+    locations.forEach((v) => {
+        const item = array[v];
+        if (item && v >= 0 && v < array.length) {
+            if (v - pos > 1) {
+                if (Math.floor(pos / width) + 1 === Math.floor(v / width)) {
+                    res.push(callbackfn(item, v, array));
+                }
+            } else if (v - pos < -1) {
+                if (Math.floor(pos / width) - 1 === Math.floor(v / width)) {
+                    res.push(callbackfn(item, v, array));
+                }
+            } else {
+                if (Math.floor(pos / width) === Math.floor(v / width)) {
+                    res.push(callbackfn(item, v, array));
+                }
+            }
+        }
+    });
+
+    return res;
+}
+
+type Node = {
+    gScore: number;
+    fScore: number;
+    edges: {
+        key: string;
+        weight: number;
+    }[];
+    cameFrom?: Node;
+};
+
+function A_star(
+    nodes: Map<string, Node>,
+    start: string,
+    goal: string,
+    h: (key: string, nodes: Map<string, Node>) => number,
+) {
+    const openSet: Map<string, Node> = new Map();
+
+    const _start = nodes.get(start);
+    if (!_start) {
+        return "fail";
+    }
+    _start.gScore = 0;
+    openSet.set(start, _start);
+
+    while (openSet.size > 0) {
+        const current = Array.from(openSet)
+            .sort((a, b) => a[1].fScore - b[1].fScore)
+            .pop();
+
+        if (!current) {
+            return "fail";
+        }
+
+        if (current[0] === goal) {
+            return current;
+        }
+
+        openSet.delete(current[0]);
+
+        for (const edge of current[1].edges) {
+            const node = nodes.get(edge.key);
+            const tentative_gScore = current[1].gScore + edge.weight;
+            if (node && tentative_gScore < node.gScore) {
+                node.cameFrom = current[1];
+                node.gScore = tentative_gScore;
+                node.fScore = h(edge.key, nodes);
+                nodes.set(edge.key, node);
+
+                if (!openSet.has(edge.key)) {
+                    openSet.set(edge.key, node);
+                }
+            }
+        }
+    }
+
+    return "end";
+}
+
+export function stage0(values: number[], width: number) {
+    const nodes: Map<string, Node> = new Map(
+        values.map((_v, i, arr) => [
+            `${i % width},${Math.floor(i / width)}`,
+            {
+                gScore: Number.POSITIVE_INFINITY,
+                fScore: Number.POSITIVE_INFINITY,
+                edges: connected(arr, width, i, (v, i) => ({
+                    key: `${i % width},${Math.floor(i / width)}`,
+                    weight: v,
+                })),
+            },
+        ]),
+    );
+
+    const res = A_star(nodes, "0,0", `${width - 1},${width - 1}`, (k) => {
+        const res = k.split(",").map((v) => parseInt(v)) as [number, number];
+        return Math.floor(values.length / width) - res[1] + (width - res[0]);
+    });
+
+    return typeof res !== "string" ? res[1].gScore : res;
+}
+
+export function stage1(values: number[], width: number) {
+    const additives = [
+        [0, 1, 2, 3, 4],
+        [1, 2, 3, 4, 5],
+        [2, 3, 4, 5, 6],
+        [3, 4, 5, 6, 7],
+        [4, 5, 6, 7, 8],
+    ];
+
+    const lines: number[][] = [];
+
+    for (let i = 0; i < values.length; i += width) {
+        lines.push(values.slice(i, i + width));
+    }
+
+    const newValues: number[] = [];
+
+    for (const add of additives) {
+        for (const line of lines) {
+            for (const num of add) {
+                newValues.push(
+                    ...line.map((v) => (v + num > 9 ? v + num - 9 : v + num)),
+                );
+            }
+        }
+    }
+
+    return stage0(newValues, width * 5);
 }
